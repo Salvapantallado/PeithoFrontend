4feb63c69d56912d252ae2d22a02a0861c8a9952
Lighuen <lighu.miranda@gmail.com>
2024-02-26 15:04:59 -0300

    Story images reuploaded

--- src/helper/StoryMock.js
+++ src/helper/StoryMock.js
@@ -1,22 +1,22 @@
 const stories = [
   {
-    url: "https://media.discordapp.net/attachments/1149813754792513619/1179518369402204370/Para_Historias_1.jpg?ex=657a1315&is=65679e15&hm=b6513ab567a65ffe689a3cdbd831fa830b7cfb47f4da20d84aa746a461d71f23&=&format=webp&width=325&height=579",
+    url: "https://media.discordapp.net/attachments/1149813754792513619/1179518369402204370/Para_Historias_1.jpg?ex=65e8d115&is=65d65c15&hm=ecf783c04134b672ec1c54f0059e904dc262b056eb7ee3f576ea63a998b536fd&=&format=webp&width=394&height=700",
     type: "img",
   },
   {
-    url: "https://media.discordapp.net/attachments/1149813754792513619/1179518370115244114/Para_Historias_2.jpg?ex=657a1315&is=65679e15&hm=16cb6b3dcd0b0571dc74c51b0e6689aef5fe9c5e05d7627415f97d74be33fc1d&=&format=webp&width=326&height=579",
+    url: "https://media.discordapp.net/attachments/1149813754792513619/1179518370115244114/Para_Historias_2.jpg?ex=65e8d115&is=65d65c15&hm=2fd023017752d2006891e67fa753cc6ede1bb9d574ae932d3107aab42712a63a&=&format=webp&width=394&height=700",
     type: "img",
   },
   {
-    url: "https://media.discordapp.net/attachments/1149813754792513619/1179518370610159637/Para_Historias_3.jpg?ex=657a1315&is=65679e15&hm=ec18fa9dea99ba5cd1171c87983dbcb2a80eb845ef7c33fcdf7878bdd8c42b47&=&format=webp&width=326&height=579",
+    url: "https://media.discordapp.net/attachments/1149813754792513619/1179518370610159637/Para_Historias_3.jpg?ex=65e8d115&is=65d65c15&hm=df6bf38661ea12faf37061a1d58d5c36bfdfa1f78e4eeadafcd8573f2fca3d74&=&format=webp&width=394&height=700",
     type: "img",
   },
   {
-    url: "https://media.discordapp.net/attachments/1149813754792513619/1179518371042177116/Para_Historias_4.jpg?ex=657a1316&is=65679e16&hm=f238a0313571ac875bb557d3ac321f02c438bab827719f51e9787225c6220df1&=&format=webp&width=326&height=579",
+    url: "https://media.discordapp.net/attachments/1149813754792513619/1179518371042177116/Para_Historias_4.jpg?ex=65e8d116&is=65d65c16&hm=d9ca8f26b5f9d698985af348e3d61c4849d9a86571e000dd6bda959e4e437234&=&format=webp&width=394&height=700",
     type: "img",
   },
   {
-    url: "https://media.discordapp.net/attachments/1149813754792513619/1179518371566473309/Para_Historias_5.jpg?ex=657a1316&is=65679e16&hm=0fd600c24d28ea6c91cae0f91f24a58c1da5908010ad00693a01eaa50534d3ab&=&format=webp&width=326&height=579",
+    url: "https://media.discordapp.net/attachments/1149813754792513619/1179518371566473309/Para_Historias_5.jpg?ex=65e8d116&is=65d65c16&hm=d3a070a85fcf93ad8bc29ef9ec2483fe0a3520b8eaccfab69b222f6e086a220f&=&format=webp&width=394&height=700",
     type: "img",
   },
 ];
