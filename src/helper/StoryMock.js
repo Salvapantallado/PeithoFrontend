4497bb034a88b24dc57f2d74053ae33b905d2ade
Lighuen Miranda <lighu.miranda@gmail.com>
2024-04-08 17:12:46 -0300

    Update StoryMock.js

--- src/helper/StoryMock.js
+++ src/helper/StoryMock.js
@@ -1,22 +1,22 @@
 const stories = [
   {
-    url: "https://media.discordapp.net/attachments/1149813754792513619/1179518369402204370/Para_Historias_1.jpg?ex=65e8d115&is=65d65c15&hm=ecf783c04134b672ec1c54f0059e904dc262b056eb7ee3f576ea63a998b536fd&=&format=webp&width=394&height=700",
+    url: "https://live.staticflickr.com/65535/53631547231_ac484091b1_c.jpg",
     type: "img",
   },
   {
-    url: "https://media.discordapp.net/attachments/1149813754792513619/1179518370115244114/Para_Historias_2.jpg?ex=65e8d115&is=65d65c15&hm=2fd023017752d2006891e67fa753cc6ede1bb9d574ae932d3107aab42712a63a&=&format=webp&width=394&height=700",
+    url: "https://live.staticflickr.com/65535/53631992315_8288b238af_c.jpg",
     type: "img",
   },
   {
-    url: "https://media.discordapp.net/attachments/1149813754792513619/1179518370610159637/Para_Historias_3.jpg?ex=65e8d115&is=65d65c15&hm=df6bf38661ea12faf37061a1d58d5c36bfdfa1f78e4eeadafcd8573f2fca3d74&=&format=webp&width=394&height=700",
+    url: "https://live.staticflickr.com/65535/53631547211_5b4efb7725_c.jpg",
     type: "img",
   },
   {
-    url: "https://media.discordapp.net/attachments/1149813754792513619/1179518371042177116/Para_Historias_4.jpg?ex=65e8d116&is=65d65c16&hm=d9ca8f26b5f9d698985af348e3d61c4849d9a86571e000dd6bda959e4e437234&=&format=webp&width=394&height=700",
+    url: "https://live.staticflickr.com/65535/53631884849_1cb9b67f42_c.jpg",
     type: "img",
   },
   {
-    url: "https://media.discordapp.net/attachments/1149813754792513619/1179518371566473309/Para_Historias_5.jpg?ex=65e8d116&is=65d65c16&hm=d3a070a85fcf93ad8bc29ef9ec2483fe0a3520b8eaccfab69b222f6e086a220f&=&format=webp&width=394&height=700",
+    url: "https://live.staticflickr.com/65535/53631884859_e8f02f9323_c.jpg",
     type: "img",
   },
 ];
